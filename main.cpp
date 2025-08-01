7e0dd17fca77cfba8b950ddba407d2d3db4cf03d
2lazycoffee2 <francisfrance255@gmail.com>
2025-08-01 16:32:57 +0200

    déplacement du personnage

--- main.cpp
+++ main.cpp
@@ -1,7 +1,48 @@
 #include <iostream> 
-
 using namespace std;
+#include <termios.h>
+#include <unistd.h>
+#include <fcntl.h>
 
+char getch()
+{
+/*
+lecture d'une touche instantannée 
+*/
+struct termios oldt, newt;
+char ch;
+tcgetattr(STDERR_FILENO, &oldt);
+newt = oldt;
+newt.c_lflag &= ~(ICANON | ECHO);
+ch = getchar();
+tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+return ch;
+}
+
+bool kbhit() {
+    termios oldt, newt;
+    int ch;
+    int oldf;
+
+    tcgetattr(STDIN_FILENO, &oldt);
+    newt = oldt;
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+    oldf = fcntl(STDIN_FILENO, F_GETFL, 0);
+    fcntl(STDIN_FILENO, F_SETFL, oldf | O_NONBLOCK);
+
+    ch = getchar();
+
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+    fcntl(STDIN_FILENO, F_SETFL, oldf);
+
+    if (ch != EOF) {
+        ungetc(ch, stdin);
+        return true;
+    }
+
+    return false;
+}
 
 
 float posPlayerX = 5.0f; // des flottants pour se balader sur les endroits ne contenant que du sol.
@@ -26,6 +67,8 @@ int grid[10][10] = { // Ici on crée une sorte de carte dans laquel on y met des
         {1,1,1,1,1,1,1,1,1,1}
 }; 
 
+
+
 void minimap()
 {
     for (int y = 0; y <= 9; y++)
@@ -47,10 +90,46 @@ void minimap()
     }
 }
 
+void inputs()
+{
+}
 
 
 int main()
 {
+    while (1)
+    {
+        if (kbhit())
+        {
+            char z = getch();
+            posPlayerY = posPlayerY - 1;
+            minimap();
+
+        }
+        else if (kbhit())
+        {
+            char q = getch();
+            posPlayerX = posPlayerX -1;
+            minimap();
+
+        }
+        else if (kbhit())
+        {
+            char s = getch();
+            posPlayerY = posPlayerY + 1;
+            minimap();
+
+        }
+        else if (kbhit())
+        {
+            char d = getch();
+            posPlayerX = posPlayerX  +1;
+            minimap();
+            
+        }
+        
+
+    }
     minimap();
     return 0;
 }
