3fda1a451b7c6d04c9bca985af7aaa31f458ef59
2lazycoffee2 <francisfrance255@gmail.com>
2025-08-01 15:49:53 +0200

    ajout de la minimap

--- main.cpp
+++ main.cpp
@@ -1,10 +1,56 @@
 #include <iostream> 
+
 using namespace std;
+
+
+
+float posPlayerX = 5.0f; // des flottants pour se balader sur les endroits ne contenant que du sol.
+float posPlayerY = 5.0f;
+
+float dirX = 0.0f;        // Direction dans laquelle le joueur regarde.
+float dirY = 1.0f;
+
+int screenWidth = 80;
+int screenHeight = 24;
+
+int grid[10][10] = { // Ici on crée une sorte de carte dans laquel on y met des 0 pour les sols sans murs et 1 pour les murs
+        {1,1,1,1,1,1,1,1,1,1},
+        {1,0,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,1,0,0,1},
+        {1,1,0,0,0,0,0,0,0,1},
+        {1,0,0,1,0,0,0,0,0,1},
+        {1,0,0,1,0,0,0,0,0,1},
+        {1,0,1,0,0,0,0,0,0,1},
+        {1,0,0,0,0,0,0,0,0,1},
+        {1,1,1,1,1,1,1,1,1,1}
+}; 
+
+void minimap()
+{
+    for (int y = 0; y <= 9; y++)
+    {
+        for (int x = 0; x<= 9; x++)
+        {
+            if (posPlayerX == x && posPlayerY == y)
+            {
+                std::cout << "P";
+            }
+            else if (grid[y][x] == 1)
+            {
+                std::cout << "#";
+            }
+            else 
+                std::cout << "."; 
+          }
+    std::cout << std::endl ;
+    }
+}
+
 
 
 int main()
 {
-
-    std::cout<<"hello world" <<std::endl;
+    minimap();
     return 0;
 }
